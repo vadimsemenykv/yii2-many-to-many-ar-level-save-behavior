d9db3c207892430836a1e6d6a6e664a499f5f1fd
Vadym Semeniuk <vadimsemenykv@gmail.com>
2016-07-18 17:01:08 +0300

    init git create ManyToMany behavior

---
+++ README.md
@@ -0,0 +1,104 @@
+Yii2 ManyToMany Active Record Level Behavior
+============================================
+Extension save many-to-many related data
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist vadymsemeniuk/yii2-many-to-many-ar-level-save-behavior "*"
+```
+
+or add
+
+```
+"vadymsemeniuk/yii2-many-to-many-ar-level-save-behavior": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+An example of usage could be:
+
+Suppose you have the following schema.
+
+```sql
+CREATE TABLE `article` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(1024) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+);
+
+CREATE TABLE `tag` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(1024) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+);
+
+CREATE TABLE `article_to_tag` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk-article_to_tag-article_id-article-id` (`article_id`),
+  KEY `fk-article_to_tag-tag_id-tag-id` (`tag_id`),
+  CONSTRAINT `fk-article_to_tag-article_id-article-id` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk-article_to_tag-tag_id-tag-id` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+)
+```
+
+Your code must look like this
+
+```php
+use adymsemenykv\manyToManyBehavior\ManyToManyArLevelSaveBehavior
+
+/**
+ * @ property Tag[] $tags
+ */
+class Article extends ActiveRecord {
+    public $tagIds = [];
+    
+    public function tableName() {
+        return 'article';
+    }
+    
+    public function getTags()
+    {
+        return $this->hasMany(Tag::className(), ['id' => 'tag_id'])
+            ->viaTable('article_to_tag', ['article_id' => 'id']);
+    }
+    
+    public function behaviors()
+    {
+        return [
+            'manyToManyBehavior' => [
+                'class' => ManyToManyArLevelSaveBehavior::className(),
+                'relations' => [
+                    'tags' => [
+                        'modelClass' => Tag::className(),
+                        'attribute' => 'tagIds',
+                        'pkColumnName' => 'id',
+                        'deleteAllRelatedEntriesBeforeSave' => true,
+                    ],
+                ],
+            ],
+        ];
+    }
+}
+```
+
+```php
+class Tag extends ActiveRecord {
+
+    public function tableName() {
+        return 'tag';
+    }
+}
+```
---
+++ src/ManyToManyArLevelSaveBehavior.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vadym Semeniuk
+ * Date: 7/18/16
+ * Time: 4:13 PM
+ */
+
+namespace vadymsemenykv\manyToManyBehavior;
+
+use yii\base\Behavior;
+use yii\base\InvalidConfigException;
+use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
+
+class ManyToManyArLevelSaveBehavior extends Behavior
+{
+    public $multiRelations = false;
+    /**
+     * Array with all many to many relations
+     * For example:
+     *      If we have model Article and model Tag.
+     *      Article and Tag relate as ManyToMany, so we have table article_to_tag to store this.
+     *      We have relation in Article that look like this:
+     *
+     *      ```php
+     *       * @ property Tag[] $tags
+     *      ```
+     *
+     *      ```php
+     *          public $tagIds = [];
+     *      ```
+     *
+     *      ```php
+     *      public function getTags()
+     *      {
+     *          return $this->hasMany(Tag::className(), ['id' => 'tag_id'])
+     *              ->viaTable('article_to_tag', ['article_id' => 'id']);
+     *      }
+     *      ```
+     *
+     *      We well use behavior config like this:
+     *
+     *          ```php
+     *          'manyToManyRelationSaveBehavior' => [
+     *              'class' => ManyToManySaveArLevelBehavior::className(),
+     *              'relations' => [
+     *                  'tags' => [
+     *                      'modelClass' => Tag::className(),
+     *                      'attribute' => 'tagIds',
+     *                      'pkColumnName' => 'id',
+     *                      'deleteAllRelatedEntriesBeforeSave' => true,
+     *                  ],
+     *              ],
+     *          ],
+     *          ```
+     *
+     * @var array
+     */
+    public $relations = [];
+
+    /**
+     * @inheritdoc
+     */
+    public function events()
+    {
+        return [
+            ActiveRecord::EVENT_AFTER_FIND => 'afterFind',
+            ActiveRecord::EVENT_AFTER_INSERT => 'afterSave',
+            ActiveRecord::EVENT_AFTER_UPDATE => 'afterSave',
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        if (empty($this->relations)) {
+            throw new InvalidConfigException('The "relations" property must be set.');
+        }
+    }
+
+    private function getProperty($relationName, $propertyName)
+    {
+        if (isset($this->relations[$relationName][$propertyName])) {
+            return $this->relations[$relationName][$propertyName];
+        } else {
+            throw new InvalidConfigException('The "' . $propertyName . '" property in relation "' . $relationName . '" must be set.');
+        }
+    }
+
+    public function afterFind()
+    {
+        foreach ($this->relations as $relationKey => $relationValue) {
+            $this->owner->{$this->getProperty($relationKey, 'attribute')} = ArrayHelper::getColumn(
+                $this->owner->{$relationKey},
+                $this->getProperty($relationKey, 'pkColumnName')
+            );
+        }
+    }
+
+    public function afterSave()
+    {
+        foreach ($this->relations as $relationKey => $relationValue) {
+            $this->owner->unlinkAll($relationKey, $this->getProperty($relationKey, 'deleteAllRelatedEntriesBeforeSave'));
+
+            /* @var \yii\db\ActiveRecord $class */
+            $class = $this->getProperty($relationKey, 'modelClass');
+            foreach ($this->owner->{$this->getProperty($relationKey, 'attribute')} as $modelId) {
+                $model = $class::findOne($modelId);
+                $this->owner->link($relationKey, $model);
+            }
+        }
+    }
+}
